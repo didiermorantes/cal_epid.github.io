c36f4cfb5023bd6ab549e4a8b31845da9e7fafc7
didiermorantes <didiermorantes@gmail.com>
2019-09-03 11:17:26 -0500

    hacemos append a la tabla general en vez del div

--- js/calendarioIngresaFecha.js
+++ js/calendarioIngresaFecha.js
@@ -153,8 +153,9 @@ let tabla_general = document.createElement("table");
    // mes.appendChild(tabla_mes);
 
             //unimos a la tabla general la tabla de cada mes generado (SI SE QUIERE DESCARGAR EL EXCEL PERO NO VISUALIZAR BIEN)
-           // tabla_general.appendChild(tabla_mes);
-           mes.appendChild(tabla_mes);
+            tabla_general.appendChild(tabla_mes);
+           //unimos a la tabla general al div de cada mes generado (SI SE QUIERE VISUALIZAR BIEN PERO NO GENERAR EXCEL)
+          // mes.appendChild(tabla_mes);
 
     //Título
     let titulo = document.createElement("caption");
