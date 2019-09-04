de4d702c69b78ebb0cd3ce424bb75837a93c0b1f
didiermorantes <didiermorantes@gmail.com>
2019-09-04 11:15:42 -0500

    insertamos saltos despues de insertada la tabla

--- js/calendarioIngresaFecha.js
+++ js/calendarioIngresaFecha.js
@@ -233,50 +233,7 @@ let tabla_general = document.createElement("table");
     document.body.appendChild(mes);
 
 
-//insertamos saltos en el div antes de insertar la tabla
-    if(m % 3 == 0 ){
 
-      if(m==3){ //solo para los primeros tres meses
-    //creamos saltos para organizar el pdf cada tres meses
-    //los agregamos al div mes
-    let salto1 = document.createElement("br");
-    let salto2 = document.createElement("br");
-    let salto3 = document.createElement("br");
-    mes.appendChild(salto1);
-    mes.appendChild(salto2);
-    mes.appendChild(salto3);
-
-
-      }
-//creamos saltos para organizar el pdf cada tres meses
-//los agregamos al div mes
-let salto1 = document.createElement("br");
-let salto2 = document.createElement("br");
-let salto3 = document.createElement("br");
-let salto4 = document.createElement("br");
-let salto5 = document.createElement("br");
-let salto6 = document.createElement("br");
-let salto7 = document.createElement("br");
-let salto8 = document.createElement("br");
-let salto9 = document.createElement("br");
-let salto10 = document.createElement("br");
-let salto11 = document.createElement("br");
-let salto12 = document.createElement("br");
-mes.appendChild(salto1);
-mes.appendChild(salto2);
-mes.appendChild(salto3);
-mes.appendChild(salto4);
-mes.appendChild(salto5);
-mes.appendChild(salto6);
-mes.appendChild(salto7);
-mes.appendChild(salto8);
-mes.appendChild(salto9);
-mes.appendChild(salto10);
-mes.appendChild(salto11);
-mes.appendChild(salto12);
-
-}
-       
 
 
 
@@ -318,6 +275,10 @@ mes.appendChild(salto12);
            div_general.appendChild(mes);
 
 
+
+
+
+
           
 
     //Título
@@ -348,7 +309,54 @@ mes.appendChild(salto12);
         dia.innerText = "";
         fila.appendChild(dia);
       }     
-    }    
+    } 
+    
+    
+//insertamos saltos en el div despúes de insertar la tabla
+if(m % 3 == 0 ){
+
+  if(m==3){ //solo para los primeros tres meses
+//creamos saltos para organizar el pdf cada tres meses
+//los agregamos al div mes
+let salto1 = document.createElement("br");
+let salto2 = document.createElement("br");
+let salto3 = document.createElement("br");
+mes.appendChild(salto1);
+mes.appendChild(salto2);
+mes.appendChild(salto3);
+
+
+  }
+//creamos saltos para organizar el pdf cada tres meses
+//los agregamos al div mes
+let salto1 = document.createElement("br");
+let salto2 = document.createElement("br");
+let salto3 = document.createElement("br");
+let salto4 = document.createElement("br");
+let salto5 = document.createElement("br");
+let salto6 = document.createElement("br");
+let salto7 = document.createElement("br");
+let salto8 = document.createElement("br");
+let salto9 = document.createElement("br");
+let salto10 = document.createElement("br");
+let salto11 = document.createElement("br");
+let salto12 = document.createElement("br");
+mes.appendChild(salto1);
+mes.appendChild(salto2);
+mes.appendChild(salto3);
+mes.appendChild(salto4);
+mes.appendChild(salto5);
+mes.appendChild(salto6);
+mes.appendChild(salto7);
+mes.appendChild(salto8);
+mes.appendChild(salto9);
+mes.appendChild(salto10);
+mes.appendChild(salto11);
+mes.appendChild(salto12);
+
+}
+   
+
   }//fin mes
 
 
