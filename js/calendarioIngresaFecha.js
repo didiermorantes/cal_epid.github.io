8a9db8bba35a3089664b649eaf057a7e080eca75
didiermorantes <didiermorantes@gmail.com>
2019-09-04 11:50:07 -0500

    corregimos mes

--- js/calendarioIngresaFecha.js
+++ js/calendarioIngresaFecha.js
@@ -224,28 +224,7 @@ let tabla_general = document.createElement("table");
   for (m = 1; m <= 12; m++) {
 //Se genera una tabla por cada més
 
-if(m==8){ //solo para el mes 8
-  //creamos saltos para organizar el pdf 
-  //los agregamos al div mes
-  let salto1 = document.createElement("br");
-  let salto2 = document.createElement("br");
-  let salto3 = document.createElement("br");
-  let salto4 = document.createElement("br");
-  let salto5 = document.createElement("br");
-  let salto6 = document.createElement("br");
-  let salto7 = document.createElement("br");
-  //Agregamos los saltos al div general para no alterar las tablas
-  mes.appendChild(salto1);
-  mes.appendChild(salto2);
-  mes.appendChild(salto3);
-  mes.appendChild(salto4);
-  mes.appendChild(salto5);
-  mes.appendChild(salto6);
-  mes.appendChild(salto7);
 
-  
-  
-    }
 
 
     //Mes
@@ -260,8 +239,30 @@ if(m==8){ //solo para el mes 8
     }
 
     document.body.appendChild(mes);
+    
 
-
+    if(m==8){ //solo para el mes 8
+      //creamos saltos para organizar el pdf 
+      //los agregamos al div mes
+      let salto1 = document.createElement("br");
+      let salto2 = document.createElement("br");
+      let salto3 = document.createElement("br");
+      let salto4 = document.createElement("br");
+      let salto5 = document.createElement("br");
+      let salto6 = document.createElement("br");
+      let salto7 = document.createElement("br");
+      //Agregamos los saltos al div general para no alterar las tablas
+      mes.appendChild(salto1);
+      mes.appendChild(salto2);
+      mes.appendChild(salto3);
+      mes.appendChild(salto4);
+      mes.appendChild(salto5);
+      mes.appendChild(salto6);
+      mes.appendChild(salto7);
+    
+      
+      
+        }
 
 
 
